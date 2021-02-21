feaf55bfc105297e5d246662e6f327c8acf52591
pritam_magadum <pritam.magadum@earlysalary.com>
2021-02-21 20:02:01 +0530

    chnages for staging brach

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Pritam's
+          Working fine
            <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
